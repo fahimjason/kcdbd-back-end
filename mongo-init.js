8ab32d8722dbe699bb53945ae79f48b6b7dbfc3d
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2024-03-20 12:11:52 +0600

    Update env for docker-compose

--- mongo-init.js
+++ mongo-init.js
@@ -1,8 +1,8 @@
 db = db.getSiblingDB('kcdbd');
 
 db.createUser({
-  user: 'admin',
-  pwd: 'password',
+  user: process.env.MONGO_INITDB_ROOT_USERNAME,
+  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
   roles: [
     {
       role: 'readWrite',
