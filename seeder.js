5279f247b1cd21da4216ede91cf5049aba94c146
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2024-04-02 16:15:56 +0600

    Update seeder.js and _data files

--- seeder.js
+++ seeder.js
@@ -8,9 +8,12 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const User = require('./models/User');
+const Ticket = require('./models/Ticket');
+const Coupon = require('./models/Coupon');
+const Workshop = require('./models/Workshop');
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI_PROD, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -20,10 +23,25 @@ const users = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
+const tickets = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/tickets.json`, 'utf-8')
+);
+
+const coupons = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/coupons.json`, 'utf-8')
+);
+
+const workshops = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/workshops.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
     try {
         await User.create(users);
+        await Ticket.create(tickets);
+        await Coupon.create(coupons);
+        await Workshop.create(workshops);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (err) {
@@ -35,6 +53,9 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await User.deleteMany();
+        await Ticket.deleteMany();
+        await Coupon.deleteMany();
+        await Workshop.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
