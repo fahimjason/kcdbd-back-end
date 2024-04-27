f1db59134edd264c157a303680505b10e495862a
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2024-04-28 00:15:11 +0600

    Update status field in CSV generation

--- utils/csv.js
+++ utils/csv.js
@@ -13,6 +13,7 @@ async function generateCSV(orders) {
             { id: 'organization', title: 'Organization' },
             { id: 'studentId', title: 'Designation/Student ID' },
             { id: 'tshirt', title: 'T-Shirt Size'},
+            { id: 'status', title: 'Status'},
             { id: 'track', title: 'Track'},
             { id: 'workshopDetails.0.title', title: 'Workshop Title' } // Access title from populated workshop
         ]
@@ -26,6 +27,7 @@ async function generateCSV(orders) {
         organization: order.organization,
         studentId: order.studentId,
         tshirt: order.tshirt,
+        status: order.status,
         track: order.track,
         'workshopDetails.0.title': order.workshopDetails?.[0]?.title ?? ''
     }));
